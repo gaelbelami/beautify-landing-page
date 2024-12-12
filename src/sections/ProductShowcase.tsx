299b6cd79b70fe227c61bfde44c46b7a5367c7a8
gaelbelami <ishimwegaelbelami@gmail.com>
2024-12-12 09:56:06 -0800

    Landing page structure and styles; update package name and dependencies

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,28 @@
+import productImage from '../assets/product-image.png';
+import pyramidImage from '../assets/pyramid.png';
+import tubeImage from '../assets/tube.png';
+import Image from 'next/image';
+
 export const ProductShowcase = () => {
-  return null;
+  return <section className='bg-gradient-to-b from-[FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
+    <div className="container">
+     <div className='section-heading'>
+     <div className='flex justify-center'>
+      <div className='tag'>
+        Boost your productivity with our powerful tools
+      </div>
+      </div>
+      <h2 className='section-title mt-5'>A more effective way to track your progress</h2>
+      <p className='section-description mt-5'>
+        Effortlessly turn your ideas into a fully functional, responsive, website in just minutes with this template.
+      </p>
+     </div>
+      <div className='relative'>
+        <Image src={productImage} alt="Product image" className='mt-10' />
+        <Image src={pyramidImage} alt="Pyramid image" className='hidden md:block absolute -top-32 -right-36' height={262} width={262} />
+        <Image src={tubeImage} alt="Tube image" className='hidden md:block absolute bottom-24 -left-36' height={248} />
+
+        </div>
+    </div>
+  </section>
 };
